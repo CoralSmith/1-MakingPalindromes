2b6baac9798000887f3c50e493e0ae8d63f87681
Coral Smith <coral.smith92@gmail.com>
2016-02-09 17:54:24 +0000

    Complete solution - untidy and without bonus

---
+++ PalidromesTest/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindromes;
+using System;
+using System.Numerics;
+
+namespace PalidromesTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Given5Returns5WithSteps()
+        {
+            int step;
+            Assert.AreEqual(5, PalindromeMaker.From(5, out step));            
+        }
+
+        [TestMethod]
+        public void Given5WithStepsReturns0Steps()
+        {
+            int step;
+            PalindromeMaker.From(5, out step);
+            Assert.AreEqual(0, step);
+        }
+
+        [TestMethod]
+        public void Given10Return11WithSteps()
+        {
+            int step;
+            Assert.AreEqual(11, PalindromeMaker.From(10, out step));
+            Assert.AreEqual(1, step);
+        }
+
+        [TestMethod]
+        public void Given24Return66()
+        {
+            int step;
+            Assert.AreEqual(66, PalindromeMaker.From(24, out step));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void Given196ThrowsException()
+        {
+            int step;
+            PalindromeMaker.From(196, out step);
+        }
+
+        [TestMethod]
+        public void GivenValReturnsPalindromeInSteps()
+        {
+            int step;
+            Assert.AreEqual(444, PalindromeMaker.From(123, out step));
+            Assert.AreEqual(1, step);
+            Assert.AreEqual(8813200023188, PalindromeMaker.From(286, out step));
+            Assert.AreEqual(23, step);
+            Assert.AreEqual(BigInteger.Parse("4478555400006996000045558744"), PalindromeMaker.From(196196871, out step));
+            Assert.AreEqual(45, step);
+        }
+    }
+}
---
+++ Palindromes/PalindromeMaker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Palindromes
+{
+    public static class PalindromeMaker
+    {
+        const int maxSteps = 100;
+        public static BigInteger From(int value, out int steps)
+        {
+            steps = 0;
+            BigInteger result = value;
+            if (value.ToString().Length > 1)
+            {
+                while (!IsPalindrome(result) && steps < maxSteps)
+                {
+                    steps++;
+                    BigInteger swap = BigInteger.Parse(string.Concat(result.ToString().Reverse()));
+                    result += swap;
+                }
+                if (!IsPalindrome(result) && steps == maxSteps) throw new ApplicationException("Possible Lychrel number");
+            }
+            return result;
+        }
+
+        private static bool IsPalindrome(BigInteger value)
+        {
+            return value == BigInteger.Parse(string.Concat(value.ToString().Reverse()));
+        }
+    }
+}
